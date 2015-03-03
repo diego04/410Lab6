3b031a36c3bd3213bf96a77aca5ca6692c4b3d11
Kevin De Asis <deasis@ualberta.ca>
2015-03-03 02:09:53 -0700

    added url for links and tag, only post tags to server remains

--- bookmarks/main/views.py
+++ bookmarks/main/views.py
@@ -8,6 +8,7 @@ from main.models import Tag
 
 # Create your views here.
 
+
 def index(request):
     context = RequestContext(request)
     
@@ -15,22 +16,23 @@ def index(request):
     
     return render_to_response('main/index.html', {'links':links}, context)
 
+
 def tags(request):
     context = RequestContext(request)
-    
+
     tags = Tag.objects.all()
-    
-    
-    return render_to_response('main/tags.html',{'tags':tags}, context)
+
+    return render_to_response('main/tags.html', {'tags':tags}, context)
 
 
-def tag(request):
+def tag(request, tag_name):
     context = RequestContext(request)
     
-    the_tag = Tag.object.get(name=tag_name)
+    the_tag = Tag.objects.get(name=tag_name)
     links = the_tag.link_set.all()
     
-    return render_to_response('main/index.html',{'links':links, 'tag_name':'#'+tag_name}, context)
+    return render_to_response('main/index.html', {'links':links, 'tag_name': the_tag}, context)
+
 
 def add_link(request):
     
